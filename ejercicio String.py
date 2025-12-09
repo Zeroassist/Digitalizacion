5947d10aaf29e332199fb991a6009f110eef84f4
Zero <ffdezcastellanos@gmail.com>
2025-12-09 10:34:58 +0100

    holi

---
+++ ejercicio String.py
@@ -0,0 +1,19 @@
+#El programa quiere poder contar cuantas
+# de una letra tiene la cadena que ha metido el usuario,
+# después de sacar el número de letras sacara por pantalla
+# los 4 últimos caracteres(tener cuidado de que no meta espacios en blanco).
+per = (str(input("Ingresa el nombre" + '\n')))
+per = per.strip()
+try:
+    lett=(str(input("Ingresa la letra solo 1" + '\n')))
+    lett.strip()
+    lett=lett[0]
+except:
+    print("Solo 1 letra")
+cont=0
+for c in per:
+    if(c==lett.lower() or c==lett.upper()):
+        cont+=1
+print("La letra "+str(lett)+" sale: "+str(cont)+" veces")
+ult:int=len(per)-3
+print(str(per[ult:]))
